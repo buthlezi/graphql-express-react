722f56c18d899c84f4a98c01fdcdb06b43f51b29
buthlezi <buthlezi03@yahoo.com>
2021-11-06 21:11:22 +0000

    Create apollo.js

--- src/server/index.js
+++ src/server/index.js
@@ -29,9 +29,7 @@ const schema = buildASTSchema(gql`
   }
 
 # Query type is a special type that lets us query the data
-# this code says that posts will give us an array of Posts
-# if you want a single Post you can query it by calling
-# post and passing in the ID
+# this code says that posts will give us an array of "Post"s
 
   type Post {
     id: ID
@@ -43,6 +41,8 @@ const schema = buildASTSchema(gql`
 // we define a Post type, which contains an id, and author, and a body.
 // we need to say what the types are for each element - here, author
 // and body both use the primitive String type, and id is an ID.
+// if you want a single Post you can query
+// it by calling post and passing in the ID
 
 const mapPost = (post, id) => post && ({ id, ...post });
 
@@ -50,6 +50,12 @@ const root = {
   posts: () => POSTS.map(mapPost),
   post: ({ id }) => mapPost(POSTS[id], id),
 };
+
+// a set of resolvers (line 49) to tell GraphQL how to handle
+// the queries - when we query posts, it will run this function
+// providing an array of all the POSTS, using their index as an ID.
+// when we query post, it expects an id and will return the post
+// at the given index.
 
 const app = express();
 app.use(cors());
@@ -62,3 +68,9 @@ app.use('/graphql', graphqlHTTP({
 const port = process.env.PORT || 4000
 app.listen(port);
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
+
+// now we can create the server - the graphqlHTTP function creates an Express
+//  server running GraphQL, which expects the resolvers as rootValue, and the
+// schema - graphiql flag is optional and will run a server for us allowing
+// us to more easily visualize the data and see the auto-generated documentation.
+// When we run app.listen, we’re starting the GraphQL server
